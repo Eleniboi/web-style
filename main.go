25316d173e77f5e9ce545ca0efe8543c8a0a1b32
somafu <omafuoloche4@gmail.com>
2026-06-23 06:01:35 +0100

    register handlers

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
-
-import(
-	"net/http"
+import (
 	"fmt"
+	"log"
+	"net/http"
 )
-
 
 func main() {
 
 	http.HandleFunc("/", homeHandler)
 	fmt.Println("server is now live on port 8080...")
-	
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 }

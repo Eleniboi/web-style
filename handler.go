bb75736239f644c15011d88995d09aa8783f5f85
somafu <omafuoloche4@gmail.com>
2026-06-23 13:10:58 +0100

    login page

--- handler.go
+++ handler.go
@@ -1,26 +1,56 @@
-package main 
-
+package main
 
 import (
-
-	"net/http"
 	"html/template"
-	
+	"net/http"
+	"strings"
 )
 
+type RegDetail struct {
+	Email string
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
-var tmpl = template.Must(template.ParseFiles("templates/index.html"))
+	var tmpl = template.Must(template.ParseFiles("templates/index.html"))
 
-	if r.Method != http.MethodGet{
+	if r.Method != http.MethodGet {
 		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	err := tmpl.Execute(w, nil)
 
-	if err != nil{
+	if err != nil {
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	var Reg_email = r.FormValue("email")
+
+	if Reg_email == "" || !strings.HasSuffix(Reg_email, `@gmail.com`) {
+
+		Data := RegDetail{
+			Email: "Address must end with '@gmail.com'",
+		}
+
+		tmpl.Execute(w, Data)
+	}
+
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+
+	var tmpl = template.Must(template.ParseFiles("templates/login.html"))
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := tmpl.Execute(w, nil)
+
+	if err != nil {
 		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
 		return
 	}
